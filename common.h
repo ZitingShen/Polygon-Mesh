eafb04cb21423ae7775e7a415310d9deecb2507a
Ziting Shen <zshen@brynmawr.edu>
2016-10-26 18:39:18 -0400

    add common.h

---
+++ common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#ifdef __APPLE__
+#include <GLFW/glfw3.h>
+#include <OpenGL/glu.h>
+#else
+#include <GLFW/glfw3.h>
+#include <GL/glu.h>
+#endif
+#include <glm/glm.hpp>
+#endif
